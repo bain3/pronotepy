700e859f6781ff81964682b82545def118a7987c
bain3 <31798786+bain3@users.noreply.github.com>
2020-02-04 15:10:22 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='pronotepy',
+                 version='0.3',
+                 description='A wrapper for the pronote "API"',
+                 url='http://www.github.com/bain3/pronotepy',
+                 author='bain',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False,
+                 python_requires='>=3.6',
+                 install_requires=['beautifulsoup4>=4.8.2',
+                                   'pycryptodome>=3.9.4',
+                                   'requests>=2.22.0',
+                                   ],
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ])
