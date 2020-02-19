f0063322910ac6492cebcc220170031e3106e176
Tom <51050533+Xiloe@users.noreply.github.com>
2020-02-19 21:26:30 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ extensions = [
 	'sphinx.ext.autodoc'
 ]
 
+# Master documentation file.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
