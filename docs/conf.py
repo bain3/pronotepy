e371a7caa22d070738b2f738d512529983cda90c
Xiloe <0theinfinity0@gmail.com>
2020-02-19 21:06:02 +0100

    theme update

--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#import sphinx_glpi_theme
-#html_theme = "glpi"
-#html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+import sphinx_glpi_theme
+html_theme = "glpi"
+html_theme_path = sphinx_glpi_theme.get_html_themes_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
