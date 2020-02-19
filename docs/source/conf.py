eb1003562efaf52fbef94f137df1770deb2cebd2
Tom <51050533+Xiloe@users.noreply.github.com>
2020-02-19 21:15:33 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,11 +46,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-import sphinx_glpi_theme
+#import sphinx_glpi_theme
 
-html_theme = "glpi"
+#html_theme = "glpi"
 
-html_theme_path = sphinx_glpi_theme.get_html_themes_path()
+#html_theme_path = sphinx_glpi_theme.get_html_themes_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
