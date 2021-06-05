0564394056f611adb4968ee7d2010cd13d7a5467
bain3 <31798786+bain3@users.noreply.github.com>
2021-06-05 13:34:47 +0200

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='pronotepy',
-                 version='1.4.0',
+                 version='1.4.1',
                  description='A wrapper for the pronote "API"',
                  url='http://www.github.com/bain3/pronotepy',
                  author='bain',
