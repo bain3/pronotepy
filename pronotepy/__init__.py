2f0c3719994d82a593d4a3964ad861cc1d678c6d
bain <bain@bain.cz>
2023-02-02 18:27:17 +0100

    bump version

--- pronotepy/__init__.py
+++ pronotepy/__init__.py
@@ -6,7 +6,7 @@ __title__ = "pronotepy"
 __author__ = "bain, Xiloe"
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2020-2022 bain, Xiloe"
-__version__ = "2.8.0"
+__version__ = "2.9.0"
 
 from .dataClasses import *
 from .clients import *
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pronotepy",
-    version="2.8.0",
+    version="2.9.0",
     description='A wrapper for the pronote "API"',
     long_description=long_description,
     long_description_content_type="text/markdown",
