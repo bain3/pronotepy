dd7737d2db092cd6388604e566207222496d8114
bain <bain@bain.cz>
2026-03-22 23:59:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pronotepy",
-    version="2.14.5",
+    version="2.14.6",
     description='A wrapper for the pronote "API"',
     long_description=long_description,
     long_description_content_type="text/markdown",
